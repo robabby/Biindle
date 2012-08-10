5913c18ccd8901b2043aea62cfd057b4040585d2
Rob Abby <robabby23@gmail.com>
2012-08-09 20:59:04 -0500

    Updated for production db

--- assets/inc/connection.inc.php
+++ assets/inc/connection.inc.php
@@ -1,14 +1,13 @@
 <?php
 function dbConnect($usertype, $connectionType = 'mysqli') {
-  // $host = 'biindleadmin.db.7625389.hostedresource.com';
-  $host = 'localhost';  
+  $host = 'biindleadmin.db.7625389.hostedresource.com';
   $db = 'biindle';
   if ($usertype  == 'read') {
-	$user = 'root';
-	$pwd = '';
+	$user = 'biindleread';
+	$pwd = 'Password#23';
   } elseif ($usertype == 'write') {
-  	$user = 'root';
-	$pwd = '';
+  	$user = 'biindleadmin';
+	$pwd = 'Password#23';
   } else {
 	exit('Unrecognized connection type');
   }
@@ -25,3 +24,30 @@ function dbConnect($usertype, $connectionType = 'mysqli') {
     }
   }
 }
+/*
+function dbConnect($usertype, $connectionType = 'mysqli') {
+  $host = 'localhost';  
+  $db = 'biindle';
+  if ($usertype  == 'read') {
+  $user = 'root';
+  $pwd = '';
+  } elseif ($usertype == 'write') {
+    $user = 'root';
+  $pwd = '';
+  } else {
+  exit('Unrecognized connection type');
+  }
+  if ($connectionType == 'mysqli') {
+  $result = new mysqli($host, $user, $pwd, $db) ;
+  if (!$result) die ('Cannot connect to database');
+  return $result;
+  } else {
+    try {
+      return new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+    } catch (PDOException $e) {
+      echo 'Cannot connect to database';
+      exit;
+    }
+  }
+}
+*/
