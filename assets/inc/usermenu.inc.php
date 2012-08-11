dfeef3feb6f1f9a776cd9e2326f31f97ea7d972d
Rob Abby <robabby23@gmail.com>
2012-08-11 00:51:24 -0500

    Updated include name

--- assets/inc/usermenu.inc.php
+++
@@ -1,5 +0,0 @@
-<ul class="nav nav-list">
-  <li><a href="/user/"><i class="icon-user"></i> Profile</a></li>
-  <li><a href="/user/settings.php"><i class="icon-edit"></i> Account Settings</a></li>
-  <li><a href="/user/messages.php"><i class="icon-envelope"></i> Messages</a></li>
-</ul>
