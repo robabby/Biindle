f917b6c64acdfc7b4d113b845b04d18ab1728805
Rob <robabby23@gmail.com>
2013-03-20 12:09:39 -0500

    Implemented AJAX log-in

--- assets/inc/register_user.inc.php
+++ assets/inc/register_user.inc.php
@@ -1,11 +1,12 @@
 <?php
+$path2root = "../..";
 if (isset($_REQUEST['register']) && $_REQUEST['register'] == 1) {
-  $username = trim($_POST['username']);
-  $password = trim($_POST['pwd']);
-  $retyped = trim($_POST['conf_pwd']);
-  $first_name = trim($_POST['first_name']);
-  $last_name = trim($_POST['last_name']);
-  $email = trim($_POST['email']);
+  $username = trim($_REQUEST['username']);
+  $password = trim($_REQUEST['pwd']);
+  $retyped = trim($_REQUEST['conf_pwd']);
+  $first_name = trim($_REQUEST['first_name']);
+  $last_name = trim($_REQUEST['last_name']);
+  $email = trim($_REQUEST['email']);
 
   require_once("$path2root/assets/classes/CheckPassword.php");
   $usernameMinChars = 6;
@@ -28,6 +29,7 @@ if (isset($_REQUEST['register']) && $_REQUEST['register'] == 1) {
     $errors[] = "Your passwords don't match.";
   }
   if (!$errors) {
+    echo "There were no errors\n";
     // include the connection file
     require_once('connection.inc.php');
     $conn = dbConnect('write');
@@ -44,37 +46,43 @@ if (isset($_REQUEST['register']) && $_REQUEST['register'] == 1) {
     $stmt->bind_param('sissss', $username, $salt, $pwd, $first_name, $last_name, $email);
     $stmt->execute();
     if ($stmt->affected_rows == 1) {
+      echo "We have effected a row\n";
   	  $success = "<p>$username has been registered. You may now <a class=\"success\" href='/log_in.php'>log in</a></p>";
-      $redirectTo = "/user/index.php?username=$username";
-      $to = "$email";
-      $from = "Biindle";
-      $subject = "Welcome to Biindle!";
-      $headers = "From: $from\r\n";
-      $message = "Thank you for signing up for Biindle.com!  You have traken your first step towards a better travelling future.\n\n Your Username: $username \n\n Your Password: $password \n\n  You can view your personal Biindle here: <a href=\"http://biindle.com/user/index.php?username=$username\">Biindle.com/$username</a>";
-      $formFields = array_keys($_POST);
-      for ($i = 0; $i < sizeof($formFields); $i++)
-      {
-          $theField = strip_tags($formFields[$i]);
-          $theValue = strip_tags($_POST[$theField]);
-          $message .= $theField;
-          $message .= " = ";
-          $message .= $theValue;
-          $message .= "\n";
-      }
-      $success = mail($to, $subject, $message, $headers);
-      if ($success)
-      {
-          header("Location: " . $redirectTo);
-      }
-      else
-      {
-          echo "An error occurred when sending the email.";
-          header("Location: " . $redirectTo);
-      }
     } elseif ($stmt->errno == 1062) {
   	  $errors[] = "$username is already in use. Please choose another username.";
+      echo "Username already in use";
     } else {
   	  $errors[] = 'Sorry, there was a problem with the database.';
+      echo "Sorry, there was a problem with the database.";
     }
   }
 }
+
+/*
+$redirectTo = "/user/index.php?username=$username";
+$to = "$email";
+$from = "Biindle";
+$subject = "Welcome to Biindle!";
+$headers = "From: $from\r\n";
+$message = "Thank you for signing up for Biindle.com!  You have traken your first step towards a better travelling future.\n\n Your Username: $username \n\n Your Password: $password \n\n  You can view your personal Biindle here: <a href=\"http://biindle.com/user/index.php?username=$username\">Biindle.com/$username</a>";
+$formFields = array_keys($_POST);
+for ($i = 0; $i < sizeof($formFields); $i++)
+{
+    $theField = strip_tags($formFields[$i]);
+    $theValue = strip_tags($_POST[$theField]);
+    $message .= $theField;
+    $message .= " = ";
+    $message .= $theValue;
+    $message .= "\n";
+}
+$success = mail($to, $subject, $message, $headers);
+if ($success)
+{
+    header("Location: " . $redirectTo);
+}
+else
+{
+    echo "An error occurred when sending the email.";
+    header("Location: " . $redirectTo);
+}
+*/
