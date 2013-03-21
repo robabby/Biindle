e93f11d32875dc2cc486b932bf322a96d09cff38
Rob Abby <robabby23@gmail.com>
2013-03-21 12:57:19 -0500

    Cleanup

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -3,9 +3,6 @@
   <script src="/assets/js/bootstrap.js"></script>
   <script src="/assets/js/jquery.easing.js"></script>
   <script>
-    $(".alert").alert()
-  </script>
-  <script>
     var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
