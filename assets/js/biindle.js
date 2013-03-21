bbe37ddf7bc5bffc0d56682f445f607945dffd10
Rob <robabby23@gmail.com>
2013-03-20 20:39:21 -0500

    Misc.

--- assets/js/biindle.js
+++ assets/js/biindle.js
@@ -1,8 +1,8 @@
 var Biindle = {};
 
 Biindle.user = {
-	username : "<?php echo $_SESSION['username']; ?>",
-	first_name : "<?php echo $_SESSION['first_name']; ?>",
-	last_name : "<?php echo $_SESSION['last_name']; ?>",
-	email : "<?php echo $_SESSION['email']; ?>"
+	username : "",
+	first_name : "",
+	last_name : "",
+	email : ""
 }
