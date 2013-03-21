49b5a27e3aa578d9e12fd477f9e14a17b2dd7614
Rob <robabby23@gmail.com>
2013-03-20 19:19:34 -0500

    Added new session variables

--- assets/inc/authenticate.inc.php
+++ assets/inc/authenticate.inc.php
@@ -1,8 +1,6 @@
 <?php
 $username = trim($_POST['username']);
 $password = trim($_POST['pwd']);
-$_SESSION['username'] = $username;
-$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
 
 // location to redirect on success
 $redirect = "/user/index.php?username=$username";
@@ -22,12 +20,32 @@ $stmt->execute();
 $stmt->fetch();
 // encrypt the submitted password with the salt and compare with stored password
 if (sha1($password . $salt) == $storedPwd) {
+  // Check for authentication
   $_SESSION['authenticated'] = 'Jethro Tull';
-  // get the time the session started
-  $_SESSION['start'] = time();
-  session_regenerate_id();
-  header("Location: $redirect");
-  exit;
+
+  // Query the rest of the users information
+  $conn = dbConnect('read');
+  $sql = "SELECT * FROM users WHERE user_id = '".$user_id."'";
+  $result = $conn->query($sql) or die(mysqli_error($conn));
+  $row = $result->fetch_assoc(); 
+
+  if(!$row) {
+  	echo "There was an error authenticating the user";
+  } else {
+  	// get the time the session started
+    $_SESSION['start'] = time();
+    session_regenerate_id();
+
+    $_SESSION['username'] = $username;
+    $_SESSION['user_id'] = $row['user_id'];
+    $_SESSION['first_name'] = $row['first_name'];
+    $_SESSION['last_name'] = $row['last_name'];
+    $_SESSION['email'] = $row['email'];
+    $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+
+    header("Location: $redirect");
+    exit;
+  }
 } else {
   // if no match, prepare error message
   $error = 'Invalid username or password';
