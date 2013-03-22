1fb63e07257d07cf94f0ab625546bf5cd3cd29d3
Rob <robabby23@gmail.com>
2013-03-21 21:16:12 -0500

    Added links from Mind Map

--- assets/inc/user_menu.inc.php
+++ assets/inc/user_menu.inc.php
@@ -10,6 +10,12 @@
 	  <li><a href="/user/index.php?username=<?php echo $username; ?>"><i class="icon-home"></i> Profile</a></li>
 	  <li><a href="/user/settings.php?username=<?php echo $username; ?>"><i class="icon-cog"></i> Account Settings</a></li>
 	  <li><a href="/user/inbox/index.php?username=<?php echo $username; ?>"><i class="icon-inbox"></i> Inbox</a></li>
+	  <li><a href="#" title="#"><i class="icon-fire"></i> Activity Feed</a></li>
+	  <li><a href="#" title="#"><i class="icon-heart"></i> Charities</a></li>
+	  <li><a href="#" title="#"><i class="icon-calendar"></i> Events</a></li>
+	  <li><a href="#" title="#"><i class="icon-bullhorn"></i> Network</a></li>
+	  <li><a href="#" title="#"><i class="icon-tags"></i> Groups</a></li>
+	  <li><a href="#" title="#"><i class="icon-globe"></i> Map</a></li>
 	  <!--<li><a href="/user/members.php?username=<?php echo $username; ?>"><i class="icon-user"></i> Members</a></li>-->
 	</ul>
 </div>
