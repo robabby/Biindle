5af612dd7713a91ac83147c9e75c87b65e0dfc15
Rob <robabby23@gmail.com>
2013-03-21 20:52:45 -0500

    Hid Members link on user_menu

--- assets/inc/user_menu.inc.php
+++ assets/inc/user_menu.inc.php
@@ -10,6 +10,6 @@
 	  <li><a href="/user/index.php?username=<?php echo $username; ?>"><i class="icon-home"></i> Profile</a></li>
 	  <li><a href="/user/settings.php?username=<?php echo $username; ?>"><i class="icon-cog"></i> Account Settings</a></li>
 	  <li><a href="/user/inbox/index.php?username=<?php echo $username; ?>"><i class="icon-inbox"></i> Inbox</a></li>
-	  <li><a href="/user/members.php?username=<?php echo $username; ?>"><i class="icon-user"></i> Members</a></li>
+	  <!--<li><a href="/user/members.php?username=<?php echo $username; ?>"><i class="icon-user"></i> Members</a></li>-->
 	</ul>
 </div>
