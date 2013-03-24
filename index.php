6af922dbfe0c7d890d4984d3be63dc0a79bf88f3
Rob <robabby23@gmail.com>
2013-03-24 10:58:35 -0500

    Updated

--- index.php
+++ index.php
@@ -35,7 +35,8 @@
     <div class="span6">
       <div class="well">
         <h2>Welcome to Biindle</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <a class="btn btn-mini btn-success pull-right" href="#">Learn More</a>
       </div>
     </div><!-- .span6 -->
     <div class="span6">
